8892cd58d97db6487039e6543cf38dfe71d234b5
equt <git@47.rs>
2020-11-03 13:17:25 +0800

    [ feat ] Add default EDITOR and PAGER

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,9 @@ and alias vf 'v (f)'
 
 set fish_greeting ''
 
+set -gx EDITOR "nvim"
+set -gx PAGER "less"
+
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
