11f10c47df00518393fb9a10b57ad458963e2435
equt <git@47.rs>
2020-11-03 12:48:48 +0800

    [ feat ] Bring back FZF theme

--- fish/config.fish
+++ fish/config.fish
@@ -26,4 +26,31 @@ has starship; and starship init fish | source
 has fnm; and fnm env | source
 has lua; source (lua $XDG_CONFIG_HOME/fish/scripts/z.lua --init fish | psub)
 
+function prompt_fzf_opts --on-event fish_prompt
+
+  if test (dark-mode status) = "off"
+    # BG                 ; Not candidate text   ; Candidate text
+    set color00 '#F0F0F4'; set color04 '#a0a0ad'; set color06 '#676880'
+    # Line no && Curr >  ; Selected >           ; Matched part
+    set color0A '#676880'; set color0C '#676880'; set color0D '#4969a3'
+  else
+    # BG                 ; Not candidate text   ; Candidate text
+    set color00 '#1d242d'; set color04 '#546478'; set color06 '#546478'
+    # Line no && Curr >  ; Selected >           ; Matched part
+    set color0A '#546478'; set color0C '#546478'; set color0D '#4d75bd'
+  end
+
+  set -gx FZF_DEFAULT_OPTS "
+      --color=bg+:$color00,bg:$color00,spinner:$color0C,hl:$color0D
+      --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
+      --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
+      --height=40% --reverse
+  "
+end
+
+alias f 'fzf --height 20% --reverse --print0 -m'
+alias fzf 'fzf --height 50% --reverse --preview-window down:50%:noborder --print0 -m \
+    --bind "ctrl-j:preview-down,ctrl-k:preview-up" --preview "bat --style=numbers \
+    --color=always {} --line-range :50"'
+
 set PATH "$XDG_DATA_HOME/cargo/bin" "$HOME/.cabal/bin" "$HOME/.local/bin" $PATH
