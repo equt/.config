2cf9e0b6a3b2c86d01efd055c901df9d6a3419f2
equt <git@47.rs>
2021-04-24 22:17:40 +0800

    [ feat ] Add Emacs package

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,3 @@
-; TODO Seperate Emacs config from packages importing
 ; TODO Add more packages, e.g., org, org-roam?, AUCTeX, RefTex
 ; TODO Spaceemacs, Doomemacs as references
 (menu-bar-mode -1)
@@ -13,6 +12,9 @@
 (setq package-user-dir (concat (getenv "XDG_CACHE_HOME") "/elpa"))
 (setq make-backup-files nil)
 
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
+
 ; TODO What does auto-save-list do in Emacs
 (setq auto-save-list-file-prefix nil)
 
@@ -20,37 +22,58 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(when (version<= "26.0.50" emacs-version )
-  (global-display-line-numbers-mode))
-
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
+
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
 
 ; TODO Bring more config from Vim
 (use-package evil
   :ensure t
   :init
   (setq evil-search-module 'evil-search)
+  (setq evil-undo-system 'undo-redo)
+  (setq evil-want-keybinding nil)
   :config
+  (define-key evil-normal-state-map (kbd "SPC m") 'neotree-toggle)
   (evil-mode))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package neotree
+  :ensure t
+  :commands neotree-toggle
+  :init
+  (setq neo-window-width 40))
 
 ; TODO Pair highlight is required
 (use-package rainbow-delimiters
   :ensure t
-  :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
+  :init
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (use-package nord-theme
   :ensure t
-  :init (load-theme 'nord t))
+  :init
+  (load-theme 'nord t))
 
 (use-package hl-todo
   :hook (prog-mode . hl-todo-mode)
   :ensure t
   :config
-    (setq hl-todo-keyword-faces
-      '(("TODO"       warning bold)
-        ("FIXME"      error bold)
-        ("NOTE"       success bold)
-        ("DEPRECATED" font-lock-doc-face bold))))
+  (setq hl-todo-keyword-faces
+    '(("TODO"       warning bold)
+      ("FIXME"      error bold)
+      ("NOTE"       success bold)
+      ("DEPRECATED" font-lock-doc-face bold))))
